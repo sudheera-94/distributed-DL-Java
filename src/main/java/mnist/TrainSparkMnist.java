70ac9e769e3b22aa578d414ab9e5ee6603f1cd78
sudheera-94 <sudheerawithanage@gmail.com>
2020-11-20 15:37:43 +0530

    Initial Commit

---
+++ src/main/java/mnist/TrainSparkMnist.java
@@ -0,0 +1,210 @@
+package mnist;
+
+import com.beust.jcommander.Parameter;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.*;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.PerformanceListener;
+import org.deeplearning4j.spark.api.TrainingMaster;
+import org.deeplearning4j.spark.impl.multilayer.SparkDl4jMultiLayer;
+import org.deeplearning4j.spark.impl.paramavg.ParameterAveragingTrainingMaster;
+import org.deeplearning4j.spark.util.SparkUtils;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedOutputStream;
+
+public class TrainSparkMnist {
+
+    public static final Logger log = LoggerFactory.getLogger(TrainSparkMnist.class);
+
+    /* --- Required Arguments -- */
+
+    @Parameter(names = {"--dataPath"}, description = "Path (on HDFS or similar) of data preprocessed by preprocessing script." +
+        " See PreprocessLocal or PreprocessSpark", required = true)
+    private String dataPath;
+
+    @Parameter(names = {"--masterIP"}, description = "Controller/master IP address - required. For example, 10.0.2.4", required = true)
+    private String masterIP;
+
+    @Parameter(names = {"--networkMask"}, description = "Network mask for Spark communication. For example, 10.0.0.0/16", required = true)
+    private String networkMask;
+
+    @Parameter(names = {"--numNodes"}, description = "Number of Spark nodes (machines)", required = true)
+    private int numNodes;
+
+    @Parameter(names = {"--avgFreq"}, description = "Number of training iterations per exploitation", required = true)
+    private int avgFreq;
+
+    /* --- Optional Arguments -- */
+
+    @Parameter(names = {"--saveDirectory"}, description = "If set: save the trained network plus evaluation to this directory." +
+        " Otherwise, the trained net will not be saved")
+    private String saveDirectory = null;
+
+    @Parameter(names = {"--sparkAppName"}, description = "App name for spark. Optional - can set it to anything to identify your job")
+    private String sparkAppName = "DL4JMnist";
+
+    @Parameter(names = {"--numEpochs"}, description = "Number of epochs for training")
+    private int numEpochs = 20;
+
+    @Parameter(names = {"--minibatch"}, description = "Minibatch size (of preprocessed minibatches). Also number of" +
+        "minibatches per worker when fitting")
+    private int minibatch = 32;
+
+    @Parameter(names = {"--numWorkersPerNode"}, description = "Number of workers per Spark node. Usually use 1 per GPU, or 1 for CPU-only workers")
+    private int numWorkersPerNode = 1;
+
+    @Parameter(names = {"--gradientThreshold"}, description = "Gradient threshold. See ")
+    private double gradientThreshold = 1E-3;
+
+    @Parameter(names = {"--port"}, description = "Port number for Spark nodes. This can be any free port (port must be free on all nodes)")
+    private int port = 40123;
+
+    public static void main(String[] args) throws Exception {
+        new TrainSparkMnist().entryPoint(args);
+    }
+
+    protected void entryPoint(String[] args) throws Exception {
+        JCommanderUtils.parseArgs(this, args);
+
+        SparkConf conf = new SparkConf();
+        conf.setAppName(sparkAppName);
+        System.out.println(conf.toDebugString());
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+
+
+        //Set up TrainingMaster for gradient sharing training
+        //Create the TrainingMaster instance
+        int examplesPerDataSetObject = 1;
+        TrainingMaster trainingMaster = new ParameterAveragingTrainingMaster.Builder(examplesPerDataSetObject)
+            .rngSeed(12345)
+            .collectTrainingStats(false)
+            .batchSizePerWorker(minibatch)              // Minibatch size for each worker
+            .averagingFrequency(avgFreq)                // Number of training iterations per exploitation
+            ///
+            ///
+            ///
+            ///
+                .build();
+
+
+        MultiLayerNetwork net = getNetwork();
+        SparkDl4jMultiLayer sparkNet = new SparkDl4jMultiLayer(sc, net, trainingMaster);
+        sparkNet.setListeners(new PerformanceListener(10, true));
+
+        //Create data loader
+        DataSetIterator mnistTrain = new MnistDataSetIterator(minibatch,true,12345);
+        DataSetIterator mnistTest = new MnistDataSetIterator(minibatch,false,12345);
+
+        //Fit the network
+        for (int i = 0; i < numEpochs; i++) {
+            log.info("--- Starting Training: Epoch {} of {} ---", (i + 1), numEpochs);
+            sparkNet.fit((JavaRDD<DataSet>) mnistTrain);
+        }
+
+        //Perform evaluation
+        Evaluation evaluation = new Evaluation(10);
+        evaluation = sparkNet.doEvaluation((JavaRDD<DataSet>) mnistTest, minibatch, evaluation)[0];
+        log.info("Evaluation statistics: {}", evaluation.stats());
+
+        if (saveDirectory != null && saveDirectory.isEmpty()) {
+            log.info("Saving the network and evaluation to directory: {}", saveDirectory);
+
+            // Save network
+            String networkPath = FilenameUtils.concat(saveDirectory, "network.bin");
+            FileSystem fileSystem = FileSystem.get(sc.hadoopConfiguration());
+            try (BufferedOutputStream os = new BufferedOutputStream(fileSystem.create(new Path(networkPath)))) {
+                ModelSerializer.writeModel(sparkNet.getNetwork(), os, true);
+            }
+
+            // Save evaluation
+            String evalPath = FilenameUtils.concat(saveDirectory, "evaluation.txt");
+            SparkUtils.writeStringToFile(evalPath, evaluation.stats(), sc);
+        }
+
+
+        log.info("----- Mnist Complete -----");
+    }
+
+    public static MultiLayerNetwork getNetwork() {
+
+        int seed = 123;
+        int nChannels = 1; // Number of input channels
+        int outputNum = 10; // The number of possible outcomes
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+            .seed(seed)
+            .l2(0.0005)
+            .weightInit(WeightInit.XAVIER)
+            .updater(new Adam(1e-3))
+            .list()
+            .layer(new ConvolutionLayer.Builder(5, 5)
+                //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters to be applied
+                .nIn(nChannels)
+                .stride(1,1)
+                .nOut(20)
+                .activation(Activation.IDENTITY)
+                .build())
+            .layer(new SubsamplingLayer.Builder(PoolingType.MAX)
+                .kernelSize(2,2)
+                .stride(2,2)
+                .build())
+            .layer(new ConvolutionLayer.Builder(5, 5)
+                //Note that nIn need not be specified in later layers
+                .stride(1,1)
+                .nOut(50)
+                .activation(Activation.IDENTITY)
+                .build())
+            .layer(new SubsamplingLayer.Builder(PoolingType.MAX)
+                .kernelSize(2,2)
+                .stride(2,2)
+                .build())
+            .layer(new DenseLayer.Builder().activation(Activation.RELU)
+                .nOut(500).build())
+            .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .nOut(outputNum)
+                .activation(Activation.SOFTMAX)
+                .build())
+            .setInputType(InputType.convolutionalFlat(28,28,1)) //See note below
+            .build();
+
+        /*
+        Regarding the .setInputType(InputType.convolutionalFlat(28,28,1)) line: This does a few things.
+        (a) It adds preprocessors, which handle things like the transition between the convolutional/subsampling layers
+            and the dense layer
+        (b) Does some additional configuration validation
+        (c) Where necessary, sets the nIn (number of input neurons, or input depth in the case of CNNs) values for each
+            layer based on the size of the previous layer (but it won't override values manually set by the user)
+
+        InputTypes can be used with other layer types too (RNNs, MLPs etc) not just CNNs.
+        For normal images (when using ImageRecordReader) use InputType.convolutional(height,width,depth).
+        MNIST record reader is a special case, that outputs 28x28 pixel grayscale (nChannels=1) images, in a "flattened"
+        row vector format (i.e., 1x784 vectors), hence the "convolutionalFlat" input type used here.
+        */
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+        return model;
+    }
+
+}
