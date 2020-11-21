07562307b007c0a526c7419ac18d9031b5f11601
sudheera-94 <sudheerawithanage@gmail.com>
2020-11-21 18:42:51 +0530

    Changed Mnist to Cifar10. Added preprocessing steps

--- src/main/java/cifar10/JCommanderUtils.java
+++ src/main/java/cifar10/JCommanderUtils.java
@@ -15,7 +15,7 @@
  * SPDX-License-Identifier: Apache-2.0
  ******************************************************************************/
 
-package mnist;
+package cifar10;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
--- src/main/java/cifar10/TrainSparkCifar10.java
+++ src/main/java/cifar10/TrainSparkCifar10.java
@@ -1,4 +1,4 @@
-package mnist;
+package cifar10;
 
 import com.beust.jcommander.Parameter;
 import org.apache.commons.io.FilenameUtils;
@@ -7,8 +7,14 @@ import org.apache.hadoop.fs.Path;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.datavec.api.io.labels.ParentPathLabelGenerator;
+import org.datavec.api.io.labels.PathLabelGenerator;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.deeplearning4j.core.loader.impl.RecordReaderFileBatchLoader;
+import org.deeplearning4j.datasets.fetchers.Cifar10Fetcher;
+import org.deeplearning4j.datasets.iterator.impl.Cifar10DataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.inputs.InputType;
@@ -22,24 +28,23 @@ import org.deeplearning4j.spark.impl.paramavg.ParameterAveragingTrainingMaster;
 import org.deeplearning4j.spark.util.SparkUtils;
 import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.activations.Activation;
-import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
-import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.nd4j.linalg.learning.config.AdaDelta;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedOutputStream;
 
-public class TrainSparkMnist {
+public class TrainSparkCifar10 {
 
-    public static final Logger log = LoggerFactory.getLogger(TrainSparkMnist.class);
+    public static final Logger log = LoggerFactory.getLogger(TrainSparkCifar10.class);
 
     /* --- Required Arguments -- */
 
-//    @Parameter(names = {"--dataPath"}, description = "Path (on HDFS or similar) of data preprocessed by preprocessing script." +
-//        " See PreprocessLocal or PreprocessSpark", required = true)
-//    private String dataPath;
+    @Parameter(names = {"--dataPath"}, description = "Path (on HDFS or similar) of data preprocessed by preprocessing script." +
+        " See PreprocessLocal or PreprocessSpark", required = true)
+    private String dataPath;
 
 //    @Parameter(names = {"--masterIP"}, description = "Controller/master IP address - required. For example, 10.0.2.4", required = true)
 //    private String masterIP;
@@ -79,7 +84,7 @@ public class TrainSparkMnist {
     private int port = 40123;
 
     public static void main(String[] args) throws Exception {
-        new TrainSparkMnist().entryPoint(args);
+        new TrainSparkCifar10().entryPoint(args);
     }
 
     protected void entryPoint(String[] args) throws Exception {
@@ -107,18 +112,29 @@ public class TrainSparkMnist {
         sparkNet.setListeners(new PerformanceListener(10, true));
 
         //Create data loader
-        DataSetIterator mnistTrain = new MnistDataSetIterator(minibatch,true,12345);
-        DataSetIterator mnistTest = new MnistDataSetIterator(minibatch,false,12345);
+        int imageHeightWidth = 32;      //64x64 pixel input
+        int imageChannels = 3;          //RGB
+        int numClasses = Cifar10Fetcher.NUM_LABELS;
+
+        PathLabelGenerator labelMaker = new ParentPathLabelGenerator();
+        ImageRecordReader rr = new ImageRecordReader(imageHeightWidth, imageHeightWidth, imageChannels, labelMaker);
+        rr.setLabels(new Cifar10DataSetIterator(1).getLabels());
+        RecordReaderFileBatchLoader loader = new RecordReaderFileBatchLoader(rr, minibatch, 1, numClasses);
+        loader.setPreProcessor(new ImagePreProcessingScaler());   //Scale 0-255 valued pixels to 0-1 range
 
         //Fit the network
+        String trainPath = dataPath + (dataPath.endsWith("/") ? "" : "/") + "train";
+        JavaRDD<String> pathsTrain = SparkUtils.listPaths(sc, trainPath);
         for (int i = 0; i < numEpochs; i++) {
             log.info("--- Starting Training: Epoch {} of {} ---", (i + 1), numEpochs);
-            sparkNet.fit((JavaRDD<DataSet>) mnistTrain);
+            sparkNet.fitPaths(pathsTrain, loader);
         }
 
         //Perform evaluation
-        Evaluation evaluation = new Evaluation(10);
-        evaluation = sparkNet.doEvaluation((JavaRDD<DataSet>) mnistTest, minibatch, evaluation)[0];
+        String testPath = dataPath + (dataPath.endsWith("/") ? "" : "/") + "test";
+        JavaRDD<String> pathsTest = SparkUtils.listPaths(sc, testPath);
+        Evaluation evaluation = new Evaluation(Cifar10DataSetIterator.getLabels(false), 5); //Set up for top 5 accuracy
+        evaluation = (Evaluation) sparkNet.doEvaluation(pathsTest, loader, evaluation)[0];
         log.info("Evaluation statistics: {}", evaluation.stats());
 
         if (saveDirectory != null && saveDirectory.isEmpty()) {
@@ -137,64 +153,60 @@ public class TrainSparkMnist {
         }
 
 
-        log.info("----- Mnist Complete -----");
+        log.info("----- Cifar10 Complete -----");
     }
 
     public static MultiLayerNetwork getNetwork() {
 
         int seed = 123;
-        int nChannels = 1; // Number of input channels
+        int channels = 3; // Number of input channels
         int outputNum = 10; // The number of possible outcomes
+        int numLabels = Cifar10Fetcher.NUM_LABELS;
+        int height = 32;
+        int width = 32;
 
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-            .seed(seed)
-            .l2(0.0005)
-            .weightInit(WeightInit.XAVIER)
-            .updater(new Adam(1e-3))
-            .list()
-            .layer(new ConvolutionLayer.Builder(5, 5)
-                //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters to be applied
-                .nIn(nChannels)
-                .stride(1,1)
-                .nOut(20)
-                .activation(Activation.IDENTITY)
-                .build())
-            .layer(new SubsamplingLayer.Builder(PoolingType.MAX)
-                .kernelSize(2,2)
-                .stride(2,2)
-                .build())
-            .layer(new ConvolutionLayer.Builder(5, 5)
-                //Note that nIn need not be specified in later layers
-                .stride(1,1)
-                .nOut(50)
-                .activation(Activation.IDENTITY)
-                .build())
-            .layer(new SubsamplingLayer.Builder(PoolingType.MAX)
-                .kernelSize(2,2)
-                .stride(2,2)
-                .build())
-            .layer(new DenseLayer.Builder().activation(Activation.RELU)
-                .nOut(500).build())
-            .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                .nOut(outputNum)
-                .activation(Activation.SOFTMAX)
-                .build())
-            .setInputType(InputType.convolutionalFlat(28,28,1)) //See note below
-            .build();
+                .seed(seed)
+                .updater(new AdaDelta())
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .weightInit(WeightInit.XAVIER)
+                .list()
+                .layer(new ConvolutionLayer.Builder().kernelSize(3,3).stride(1,1).padding(1,1).activation(Activation.LEAKYRELU)
+                        .nIn(channels).nOut(32).build())
+                .layer(new BatchNormalization())
+                .layer(new SubsamplingLayer.Builder().kernelSize(2,2).stride(2,2).poolingType(SubsamplingLayer.PoolingType.MAX).build())
 
-        /*
-        Regarding the .setInputType(InputType.convolutionalFlat(28,28,1)) line: This does a few things.
-        (a) It adds preprocessors, which handle things like the transition between the convolutional/subsampling layers
-            and the dense layer
-        (b) Does some additional configuration validation
-        (c) Where necessary, sets the nIn (number of input neurons, or input depth in the case of CNNs) values for each
-            layer based on the size of the previous layer (but it won't override values manually set by the user)
+                .layer(new ConvolutionLayer.Builder().kernelSize(1,1).stride(1,1).padding(1,1).activation(Activation.LEAKYRELU)
+                        .nOut(16).build())
+                .layer(new BatchNormalization())
+                .layer(new ConvolutionLayer.Builder().kernelSize(3,3).stride(1,1).padding(1,1).activation(Activation.LEAKYRELU)
+                        .nOut(64).build())
+                .layer(new BatchNormalization())
+                .layer(new SubsamplingLayer.Builder().kernelSize(2,2).stride(2,2).poolingType(SubsamplingLayer.PoolingType.MAX).build())
 
-        InputTypes can be used with other layer types too (RNNs, MLPs etc) not just CNNs.
-        For normal images (when using ImageRecordReader) use InputType.convolutional(height,width,depth).
-        MNIST record reader is a special case, that outputs 28x28 pixel grayscale (nChannels=1) images, in a "flattened"
-        row vector format (i.e., 1x784 vectors), hence the "convolutionalFlat" input type used here.
-        */
+                .layer(new ConvolutionLayer.Builder().kernelSize(1,1).stride(1,1).padding(1,1).activation(Activation.LEAKYRELU)
+                        .nOut(32).build())
+                .layer(new BatchNormalization())
+                .layer(new ConvolutionLayer.Builder().kernelSize(3,3).stride(1,1).padding(1,1).activation(Activation.LEAKYRELU)
+                        .nOut(128).build())
+                .layer(new BatchNormalization())
+                .layer(new ConvolutionLayer.Builder().kernelSize(1,1).stride(1,1).padding(1,1).activation(Activation.LEAKYRELU)
+                        .nOut(64).build())
+                .layer(new BatchNormalization())
+                .layer(new ConvolutionLayer.Builder().kernelSize(1,1).stride(1,1).padding(1,1).activation(Activation.LEAKYRELU)
+                        .nOut(numLabels).build())
+                .layer(new BatchNormalization())
+
+                .layer(new SubsamplingLayer.Builder().kernelSize(2,2).stride(2,2).poolingType(SubsamplingLayer.PoolingType.AVG).build())
+
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .name("output")
+                        .nOut(numLabels)
+                        .dropOut(0.8)
+                        .activation(Activation.SOFTMAX)
+                        .build())
+                .setInputType(InputType.convolutional(height, width, channels))
+                .build();
 
         MultiLayerNetwork model = new MultiLayerNetwork(conf);
         model.init();
