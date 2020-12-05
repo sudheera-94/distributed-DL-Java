9de961a9f8c15b111e32dd0e90a4717eebcc51bf
sudheera-94 <sudheerawithanage@gmail.com>
2020-12-05 15:10:03 +0530

    Cifar10 Range test part done. Has to test and debug.

--- src/main/java/org/DistributedDL/examples/cifar10/cifar10CyclicLr/Cifar10LrRangeTest.java
+++ src/main/java/org/DistributedDL/examples/cifar10/cifar10CyclicLr/Cifar10LrRangeTest.java
@@ -1,4 +1,125 @@
 package org.DistributedDL.examples.cifar10.cifar10CyclicLr;
 
+import org.apache.log4j.BasicConfigurator;
+import org.deeplearning4j.api.storage.StatsStorage;
+import org.deeplearning4j.eval.IEvaluation;
+import org.deeplearning4j.nn.api.Model;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.optimize.listeners.EvaluativeListener;
+import org.deeplearning4j.optimize.listeners.callbacks.EvaluationCallback;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.stats.StatsListener;
+import org.deeplearning4j.ui.storage.InMemoryStatsStorage;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static org.DistributedDL.examples.mnist.mnistTraditional.MnistSpark.getDataSetIterator;
+
 public class Cifar10LrRangeTest {
+
+    public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
+
+        int batchSize = 100;
+        int numEpochs = 1;
+        int height = 32;
+        int width = 32;
+        int channels = 3;
+        int outputNum = 10;
+        int rngseed = 123;
+        int evaluationListenerFreq = 200;
+
+        // Loading training data
+        System.out.println("Data load and vectorization...");
+
+        // Initialize the training set iterator
+        DataSetIterator trainIter = getDataSetIterator("cifar10_png/training", rngseed, height, width,
+                channels, batchSize, outputNum);
+
+        // Pixel values from 0-255 to 0-1 (min-max scaling)
+        DataNormalization scaler = new ImagePreProcessingScaler(0, 1);
+        scaler.fit(trainIter);
+        trainIter.setPreProcessor(scaler);
+
+        // Initialize the testing set iterator
+        DataSetIterator iterTest = getDataSetIterator("cifar10_png/testing", rngseed, height, width,
+                channels, batchSize, outputNum);
+        scaler.fit(iterTest);
+        iterTest.setPreProcessor(scaler);
+
+        //Initialize the user interface backend
+        UIServer uiServer = UIServer.getInstance();
+
+        //Configure where the network information (gradients, activations, score vs. time etc) is to be stored
+        //Then add the StatsListener to collect this information from the network, as it trains
+        StatsStorage statsStorage = new InMemoryStatsStorage();   //Alternative: new FileStatsStorage(File) - see UIStorageExample
+        int listenerFrequency = 1;
+
+        // Network configuration
+        Cifar10LrRangeTestTrainingConfig cifar10LrRangeTestConfig =
+                new Cifar10LrRangeTestTrainingConfig(batchSize, numEpochs);
+        MultiLayerConfiguration conf = cifar10LrRangeTestConfig.getArchitecture();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+        // Creating a JSON Array to store EvaluativeListener accuracy
+        JSONArray EvaluativeListenerAccuracyArray = new JSONArray();
+
+        // Setting evaluative listener for get accuracy and stats listener for UI.
+        EvaluativeListener evalListener = new EvaluativeListener(iterTest, evaluationListenerFreq);
+        EvaluationCallback evalCallback = new EvaluationCallback() {
+            @Override
+            public void call(EvaluativeListener listener, Model model, long invocationsCount, IEvaluation[] evaluations) {
+                // Reading the evaluation listener results as a JSON string
+                String evaluationJson = evaluations[0].toJson();
+                JSONObject evalJsonObj = new JSONObject(evaluationJson);
+
+                // Getting # testing data used in evaluation listener
+                int dataCount = evalJsonObj.getInt("numRowCounter");
+                // Getting # true positives
+                JSONObject truePositiveObject = evalJsonObj.getJSONObject("truePositives");
+                int truePositiveCount = truePositiveObject.getInt("totalCount");
+                // Getting the accuracy of the listener
+                float accuracy = (float) truePositiveCount / (float) dataCount;
+
+                // Creating a JSON object with accuracy and the iteration
+                JSONObject EvaluativeListenerAccuracyObject = new JSONObject();
+                EvaluativeListenerAccuracyObject.put("iteration", invocationsCount * evaluationListenerFreq);
+                EvaluativeListenerAccuracyObject.put("accuracy", accuracy);
+                EvaluativeListenerAccuracyArray.put(EvaluativeListenerAccuracyObject);
+            }
+        };
+        evalListener.setCallback(evalCallback);
+
+        model.setListeners(evalListener,
+                new StatsListener(statsStorage, listenerFrequency));
+
+        //Attach the StatsStorage instance to the UI: this allows the contents of the StatsStorage
+        // to be visualized
+        uiServer.attach(statsStorage);
+
+        // Training the network
+        for (int i = 0; i < numEpochs; i++) {
+            model.fit(trainIter);
+            System.out.println("*** Completed epoch " + i + " ***");
+        }
+
+        // Getting EvaluativeListenerAccuracyArray into a text file
+        try (FileWriter file = new FileWriter("EvaluativeListenerAccuracyArray.json")) {
+            file.write(EvaluativeListenerAccuracyArray.toString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/main/java/org/DistributedDL/examples/cifar10/cifar10CyclicLr/Cifar10LrRangeTestTrainingConfig.java
+++ src/main/java/org/DistributedDL/examples/cifar10/cifar10CyclicLr/Cifar10LrRangeTestTrainingConfig.java
@@ -1,4 +1,130 @@
 package org.DistributedDL.examples.cifar10.cifar10CyclicLr;
 
+import org.DistributedDL.CyclicLr.lrRangeTestScheduleGenerator;
+import org.DistributedDL.StandardArchitectures.LeNet5Architecture;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.LearningRatePolicy;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.*;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.util.Map;
+
 public class Cifar10LrRangeTestTrainingConfig {
+
+
+    private int rangeTestEpochCount = 4;
+    private double maxLr = 0.02;
+    private int batchSize = 100;
+    private Map<Integer, Double> lrSchedule;
+
+    public Map<Integer, Double> getLrSchedule() {
+        return lrSchedule;
+    }
+
+    public int getRangeTestEpochCount() {
+        return rangeTestEpochCount;
+    }
+
+    public void setRangeTestEpochCount(int rangeTestEpochCount) {
+        this.rangeTestEpochCount = rangeTestEpochCount;
+    }
+
+    // Constructors
+    public Cifar10LrRangeTestTrainingConfig(int batchSize, int rangeTestEpochCount) {
+        this.batchSize = batchSize;
+        this.rangeTestEpochCount = rangeTestEpochCount;
+    }
+
+    public Cifar10LrRangeTestTrainingConfig(int batchSize, int rangeTestEpochCount, double maxLr) {
+        this(batchSize, rangeTestEpochCount);
+        this.maxLr = maxLr;
+    }
+
+    // Other Methods
+    public MultiLayerConfiguration getArchitecture() {
+
+        int iterations = 1; // Number of training iterations
+        int nChannels = 3; // Number of input channels
+        int outputNum = 10; // The number of possible outcomes
+        this.setLrSchedule();
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(123)
+                .iterations(iterations)     // Training iterations per minibatch
+
+                // The base learning rate, momentum and the weight decay of the network.
+                .learningRate(0.001d)        // Learning rate
+                .biasLearningRate(0.002d)
+                .updater(new Nesterovs(0.9d))
+                .l2(0.004d)
+
+                .learningRateDecayPolicy(LearningRatePolicy.Schedule)
+                .learningRateSchedule(lrSchedule)
+
+                .weightInit(WeightInit.XAVIER)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .list()
+                .layer(0, new ConvolutionLayer.Builder(5, 5)
+                        .nIn(nChannels)
+                        .name("conv1")
+                        .nOut(32)
+                        .padding(2,2)
+                        .stride(1,1)
+                        .build())
+                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .name("pool1")
+                        .kernelSize(3, 3)
+                        .stride(2, 2)
+                        .build())
+                .layer(2, new ActivationLayer.Builder()
+                        .activation(Activation.RELU)
+                        .name("relu1").build())
+                .layer(3, new ConvolutionLayer.Builder(5, 5)
+                        .name("conv2")
+                        .nOut(32)
+                        .padding(2,2)
+                        .stride(1,1)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(5, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.AVG)
+                        .name("pool2")
+                        .kernelSize(3, 3)
+                        .stride(2, 2)
+                        .build())
+                .layer(6, new ConvolutionLayer.Builder(5, 5)
+                        .name("conv3")
+                        .nOut(64)
+                        .padding(2,2)
+                        .stride(1,1)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(7, new DenseLayer.Builder()
+                        .nOut(64).build())
+                .layer(8, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        // Cross entropy loss
+                        .nOut(outputNum)
+                        .activation(Activation.SOFTMAX)
+                        .build())
+                .setInputType(InputType.convolutionalFlat(32, 32, 3))
+                .backprop(true).pretrain(false).build();
+
+        return conf;
+
+    }
+
+    private void setLrSchedule() {
+
+        Map<Integer, Double> lrScheduleNew =
+                lrRangeTestScheduleGenerator.getSchedule(this.maxLr, LeNet5Architecture.getTrainSize(),
+                        this.batchSize, this.rangeTestEpochCount);
+        this.lrSchedule = lrScheduleNew;
+
+    }
+
 }
