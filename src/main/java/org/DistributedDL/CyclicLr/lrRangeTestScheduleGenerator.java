e598550ed8df1071954645ae1ab24bd8dcd3eec5
sudheera-94 <sudheerawithanage@gmail.com>
2020-12-06 13:06:51 +0530

    Added descriptions to the lrRangeTestScheduleGenerator

--- src/main/java/org/DistributedDL/CyclicLr/lrRangeTestScheduleGenerator.java
+++ src/main/java/org/DistributedDL/CyclicLr/lrRangeTestScheduleGenerator.java
@@ -3,15 +3,35 @@ package org.DistributedDL.CyclicLr;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Before scheduling a Cyclic learning rate This Learning Rate Range test
+ * Provides a Map<Integer, Double> which has <iteration, learning rate>
+ * to the expected RangeTestConfig class.
+ */
 public class lrRangeTestScheduleGenerator {
 
     private static double lrResolution = 0.0001d;
     private static double minLr = 0.0001d;
 
+    /**
+     * Set the minimum learning rate to LR range test schedule generator.
+     *
+     * @param minLr minimum learning rate.
+     */
     public static void setMinLr(double minLr) {
         lrRangeTestScheduleGenerator.minLr = minLr;
     }
 
+    /**
+     * Returns the expected learning rate schedule. Uses private getLrArray
+     * method to get learning rate array and getIterArray
+     *
+     * @param maxLr               maximum learning rate in the tested range
+     * @param trainSize           number of training data points
+     * @param batchSize           training data points in a batch
+     * @param rangeTestEpochCount number of range test epochs
+     * @return learning rate schedule for learning rate range test
+     */
     public static Map<Integer, Double> getSchedule(double maxLr, int trainSize, int batchSize,
                                                    int rangeTestEpochCount) {
 
@@ -27,6 +47,14 @@ public class lrRangeTestScheduleGenerator {
 
     }
 
+    /**
+     * Provides an array of learning rates starting from minimum learning rate to
+     * maximum learning rate as lrResolution field as the step size.
+     *
+     * @param maxLr      maximum learning rate
+     * @param lrArrayLen length of the array of learning rates
+     * @return Array of learning rates to perform LR range test
+     */
     private static double[] getLrArray(double maxLr, int lrArrayLen) {
 
         double[] lrArray = new double[lrArrayLen];
@@ -40,6 +68,18 @@ public class lrRangeTestScheduleGenerator {
 
     }
 
+    /**
+     * Provides an array of iterations with respect to the array of learning rates. Helps
+     * to set the correct learning rate in the correct iteration.
+     *
+     * @param lrArray             array of learning rates
+     * @param lrArrayLen          length of the array of learning rates
+     * @param trainSize           number of training data points
+     * @param batchSize           training data points in a batch
+     * @param rangeTestEpochCount number of range test epochs
+     * @param maxLr               maximum learning rate in the tested range
+     * @return array of iteration numbers
+     */
     private static int[] getIterArray(double[] lrArray, int lrArrayLen, int trainSize, int batchSize,
                                       int rangeTestEpochCount, double maxLr) {
 
