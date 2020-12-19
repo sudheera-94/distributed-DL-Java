a97bff4f908c6ab9218fc255707e69dd5cd06d12
sudheera-94 <sudheerawithanage@gmail.com>
2020-12-19 15:50:48 +0530

    Started adding Vgg16Architecture

---
+++ src/main/java/org/DistributedDL/StandardArchitectures/Vgg16Architecture.java
@@ -0,0 +1,12 @@
+package org.DistributedDL.StandardArchitectures;
+
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+
+public class Vgg16Architecture extends BaseArchitecture {
+
+    public Vgg16Architecture(int nChannels, int nClasses, int trainSize) {
+        super(nChannels, nClasses, trainSize);
+    }
+
+    public static MultiLayerConfiguration
+}
